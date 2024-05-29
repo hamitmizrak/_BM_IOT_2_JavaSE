d731c52d0776c04c6f02f6274a33e836b121b3ee
Hamit Mizrak <hamitmizrak@gmail.com>
2024-05-29 16:00:48 +0300

    examples

--- src/main/java/com/hamitmizrak/_11_3_For.java
+++ src/main/java/com/hamitmizrak/_11_3_For.java
@@ -1,11 +1,21 @@
 package com.hamitmizrak;
 
-/*
-1 ile 10 arasında rastgele 8 tane sayının toplamını yapan Java algoritması , uygulama yapınız ?
+import java.util.Random;
 
+/*
+1 ile 9 arasında rastgele 8 tane sayının toplamını yapan Java algoritması , uygulama yapınız ?
 */
 public class _11_3_For {
     public static void main(String[] args) {
+        int sum=0;
 
+        // Loop
+        for (int i = 1; i <=8 ; i++) {
+            Random rand = new Random();
+            int randomNum = rand.nextInt(9)+1;
+            System.out.print(randomNum+" ");
+            sum=sum+randomNum;
+        }
+        System.out.println("\nToplam:" +sum);
     }
 }
