13876fbbb04cc44759c839ead72e261a7080c128
Hamit Mizrak <hamitmizrak@gmail.com>
2024-05-30 17:28:03 +0300

    Final

---
+++ src/main/java/com/hamitmizrak/_16_1_Final.java
@@ -0,0 +1,17 @@
+package com.hamitmizrak;
+
+// 3-)
+// classlarda final yazarsam: extends(katılımlama) yapamayız..
+public final class _16_1_Final {
+  final int data=44;
+// Final: Son
+  /*
+  1-)
+  final int data=44; => Buradaki data'yı değiştiremezsiniz.
+
+  2-)
+  Eğer Metotta yazarsak; Override yapamazsınız.
+  */
+  public final void deneme(){}
+
+}
