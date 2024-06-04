9a6756cf0811a7fa91b7b91dbb01d69974235b81
Hamit Mizrak <hamitmizrak@gmail.com>
2024-06-04 11:04:39 +0300

    File Create

--- src/main/java/project/file/FileData.java
+++ src/main/java/project/file/FileData.java
@@ -1,10 +1,45 @@
 package project.file;
 
+import java.io.File;
+import java.util.Date;
+import java.util.UUID;
+
 public class FileData {
 
     // Variable
+    private String id;
+    private Date systemCreatedDate;
+
+    //private java.io.File file;
+    private File file;
+    private String fileName;
+    private String url;
 
     // Constructor
+    public  FileData() {
+        this.id= UUID.randomUUID().toString();
+        this.systemCreatedDate = new Date(System.currentTimeMillis());
+
+        this.url=FilePathUrl.FILE_PATH;
+        this.file= new File(url);
+
+        // File Create
+        try{
+            // Eğer benim belirlediğim dosya yoksa yeni dosya ekle
+            if(file.exists()){
+                System.err.println(url+ " böyle bir dosya mevcuttur tekrar oluşturulamaz.");
+            }else{
+                file.createNewFile();
+                System.out.println(url+ " dosyanız oluşturuldu.");
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        // Bu dosya 1 yıl sonra kendi kendine silinsin.
+        // delete()
+    }
+
 
     /////////////////////////////////
     // Tarih
@@ -24,5 +59,10 @@ public class FileData {
     /////////////////////////////////
     // GETTER AND SETTER
 
+    public static void main(String[] args) {
+        // System.out.println(UUID.randomUUID().toString());
+        FileData fd=new FileData();
+
+    }
 
 } //end Class
--- src/main/java/project/file/FilePathUrl.java
+++ src/main/java/project/file/FilePathUrl.java
@@ -2,6 +2,10 @@ package project.file;
 
 public class FilePathUrl {
 
+    // java.io.IOException: Sistem belirtilen yolu bulamıyor
+    // Aşağıdaki url olan dizin eklenmemiş demektir.
     // Const
     public static final String FILE_PATH="C:\\io\\bm\\atm.txt";
+
+
 }
